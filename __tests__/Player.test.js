b7acb57966eacbe17be8745c19862914c6289d87
J0J0C0DING <102717784+J0J0C0DING@users.noreply.github.com>
2022-06-28 18:17:32 -0500

    Added prototype for player getStats and getInventory

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -13,5 +13,25 @@ test('Creates a player object', () => {
   expect(player.health).toEqual(expect.any(Number));
   expect(player.strength).toEqual(expect.any(Number));
   expect(player.agility).toEqual(expect.any(Number));
+
   expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
+});
+
+test('Gets players stats as an object', () => {
+  const player = new Player('Dave');
+
+  expect(player.getStats()).toHaveProperty('potions');
+  expect(player.getStats()).toHaveProperty('health');
+  expect(player.getStats()).toHaveProperty('strength');
+  expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('Gets inventory from player or returns false', () => {
+  const player = new Player('Dave');
+
+  expect(player.getInventory()).toEqual(expect.any(Array));
+
+  player.inventory = [];
+
+  expect(player.getInventory()).toEqual(false);
 });
